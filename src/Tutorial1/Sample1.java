c358796dc9ee5df5c7c3700dd73840688b8a3c11
user <pooja.petkar123@gmail.com>
2022-09-02 09:03:22 +0530

    First commit

---
+++ src/Tutorial1/Sample1.java
@@ -0,0 +1,10 @@
+package Tutorial1;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		System.out.println("Sample1 is created");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Gitrepost3 {
+}

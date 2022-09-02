7d32bebfb69844d3d315d51a3955d301d7d65fb8
poojaspetkar <112603351+poojaspetkar@users.noreply.github.com>
2022-09-02 15:33:58 +0530

    Update PIMPage.java

--- src/PIMPageLayer/PIMPage.java
+++ src/PIMPageLayer/PIMPage.java
@@ -4,7 +4,7 @@ public class PIMPage {
 
 	public static void main(String[] args) {
 		System.out.println("PIMPage functionality is created");
-
+		System.out.println("new employee created");
 	}
 
 }
